bd3bdec25e4a3b8ac44b31d4ebb4ccd1551d745e
Ian Gorse <ian.gorse@outlook.com>
2019-07-31 09:55:16 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,75 @@
+# Git Release Notes
+
+Generate release notes based on git history.
+
+## Getting Started
+#### Warning
+The Templite engine runs untrusted python code. Please examine template files before using them
+
+### Prerequisites
+
+Python and Git must be installed
+
+### Installing
+
+Clone the repository
+
+```
+git clone https://github.com/iangorse/GRN.git
+```
+
+Change to the directory
+
+```
+cd GRN
+```
+
+### Usage
+
+```
+python grn.py --help
+
+usage: grn.py [-h] [--gitdir GITDIR] template
+
+positional arguments:
+  template         the template file to use
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --gitdir GITDIR  the git repository path to build release notes against
+```
+
+#### Examples
+
+Build a markdown representation of the git log
+
+```
+python grn.py markdown
+```
+
+Build a HTML represenation of a git log of a different repository
+```
+python grn.py html --gitdir "c:\another repo\.git"
+```
+
+#### Warning
+The Templite engine runs untrusted python code. Please examine template files before using them
+
+## Built With
+
+* [Python 3.6](https://www.python.org/) - Python
+* [Templite](http://code.activestate.com/recipes/496702/) - Simple Template Engine for Python
+
+
+
+## Authors
+
+* **Ian Gorse** - *Initial work* - [Ian Gorse](https://github.com/iangorse)
+
+## Acknowledgments
+
+This project was inspired by https://github.com/ariatemplates/git-release-notes
+
+
+
+
---
+++ grn.py
@@ -0,0 +1,68 @@
+from templite import Templite
+import subprocess
+import argparse
+import os
+import sys
+
+def parseArguments():
+    ''' Parse the command line arguments '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("template", help="the template file to use")
+    parser.add_argument("--gitdir", help="the git repository path to build release notes against")
+    args = parser.parse_args()    
+    return args
+
+def ExcuteGitLog(git_args):
+    ''' Execute the git log command'''
+    logs = subprocess.run(git_args, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    if logs[:6] == "fatal:" or logs == '':
+        print(logs)
+        exit(1)
+    return logs
+
+def ParseLogs(logs):
+    ''' Parses the git log, storing results into a list '''
+    commits = []
+
+    for result in logs.split('\n'):
+        commit_data = result.split(';')
+        date_data = commit_data[2].split(' ')
+
+        commit_hash = commit_data[0]
+        commit_author = commit_data[1]
+        commit_date = date_data[1] + " " + date_data[2] + " " + date_data[4]
+        commit_message = result[result.find(' : ')+3:]    
+        commit = [commit_hash, commit_date, commit_message, commit_author]
+        commits.append(commit) 
+    return commits 
+
+def GetTemplate(template):
+    ''' Open the template file '''
+
+    # ENSURE THE TEMPLATE FILE EXISTS BEFORE CONTINUING
+    template_path = os.path.join("templates", template + ".txt")
+    if os.path.exists(template_path) == False:
+        print("ERROR: Cannot find template " + template_path + " Please ensure you have used the correct template file")
+        sys.exit(1)
+
+    # OPEN THE TEMPLATE DOCUMENT
+    content = ""
+    with open(template_path, "r") as file:
+        content = file.read()
+    return content        
+
+if __name__ == "__main__":
+    git_args = ['git', 'log', '--pretty=format:%h;%an;%cd : %s']
+
+    args = parseArguments()
+    if args.gitdir:
+        git_args.insert(1, '--git-dir')
+        git_args.insert(2, args.gitdir)
+
+    logs = ExcuteGitLog(git_args)
+    commits = ParseLogs(logs)
+    content = GetTemplate(args.template)
+
+    t = Templite(content)
+    print( t(commitlist=commits))
+    
